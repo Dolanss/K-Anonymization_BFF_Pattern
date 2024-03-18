747daef3bc23ec35c8619af870e5f78f0e3217b1
Gabriel Felipe <107888818+Dolanss@users.noreply.github.com>
2024-03-18 10:00:00 -0300

    test: add AnonymizationServiceApplicationTests

---
+++ anonymization-service/src/test/java/com/dataanon/anonymization/AnonymizationServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dataanon.anonymization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AnonymizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
